143dcd79f81db3e35b44dad9f9d741fdcdb98739
salman608 <salman608@gmail.com>
2022-11-05 19:55:08 +0600

    category designed

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\CategoryRequest;
 use App\Models\Category;
 use Illuminate\Http\Request;
 
@@ -35,9 +36,17 @@ class CategoryController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(CategoryRequest $request)
     {
-        //
+        $image=$request->file('image')->store('public/category');
+        Category::create([
+            'name'         =>$request->name,
+            'description'  =>$request->description,
+            'image'        =>$request->image, 
+
+        ]);
+
+        return to_route('categories.index');
     }
 
     /**
--- resources/views/admin/category/create.blade.php
+++ resources/views/admin/category/create.blade.php
@@ -19,16 +19,17 @@
 
     <div class="p-6 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
         
-<form>
+<form >
+    {{-- @csrf --}}
     
   
         <div class="mb-6">
             <label for="default-input" class="block mb-2 text-sm font-medium text-white">Category Name</label>
-            <input type="text" id="default-input" class="mb-2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+            <input type="text"  id="default-input" class="mb-2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
         </div>
         <div class="mb-6 mt-4 ">
            <label for="default-input" class="block mb-2 text-sm font-medium text-white">Description</label>
-           <textarea name="" id="" cols="30" rows="10" class="mt-2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"></textarea>
+           <textarea id="" cols="30" rows="10" class="mt-2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"></textarea>
         </div>
         <div class="mb-6 mt-4">
             <label for="default-input" class="block mb-2 text-sm font-medium text-white">Upload Image</label>
@@ -39,7 +40,7 @@
         <div class="mt-4">
             <button type="submit" class="bg-indigo-500 px-2 py-2 hover-bg-indigo-700 text-white rounded-lg">Store</button>
         </div>
-
+        
     
 </form>
 
